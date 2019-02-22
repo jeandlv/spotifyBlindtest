f1dfc4a4d4e5c7cf935559fc575c3e3533403f42
Jean Dlv <Jean@linux.home>
2019-02-22 19:09:28 +0100

    Add blidtest

--- blindtest/src/App.js
+++ blindtest/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Sound from 'react-sound';
+
 
 class App extends Component {
   constructor(props) {
@@ -11,6 +13,15 @@ class App extends Component {
   handleTracksChange(tracksLoaded, tracks) {
     console.log('tracksLoaded :', tracksLoaded)
     console.log('tracks :', tracks)
+    let trackNumber = 0;
+    while (trackNumber < tracks.length) {
+      if(tracks[trackNumber].audioLink === null) {
+        tracks.splice(trackNumber, 1);
+      }
+      else {
+        trackNumber++;
+      }
+    }
     this.setState({tracksLoaded : tracksLoaded, tracks : tracks})
   }
 
@@ -29,10 +40,19 @@ class App extends Component {
         </div>
       );
     } else {
+      if(tracks.length === 0 || tracks === undefined) {
+        return(
+          <div>
+            <h1>Welcome to the Spotify Blindtest</h1>
+            <h2>Unvalid API KEY</h2>
+          </div>
+        );
+      }
       return (
         <div className="App">
           <h1>Welcome to the Spotify Blindtest</h1>
-          <TrackTable tracks={tracks}/>
+          {/*<TrackTable tracks={tracks}/>*/}
+          <BlindtestGame tracks={tracks}/>
         </div>
       );
     }
@@ -66,7 +86,7 @@ class TracksLoader extends Component {
     let loadedtracks
     axios({
       method:'get',
-      url:' https://api.spotify.com/v1/me/tracks?limit=10',
+      url:' https://api.spotify.com/v1/playlists/21THa8j9TaSGuXYNBU5tsC/tracks?limit=50',
       headers: {
         "Accept" : "application/json",
         "Content-Type" : "application/json",
@@ -108,6 +128,139 @@ class TracksLoader extends Component {
   }
 }
 
+class BlindtestGame extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("In blindtest game constructor")
+    console.log("this game tracks : ", this.props.tracks)
+    this.hasAnswered = this.hasAnswered.bind(this);
+    this.state = { 
+      tracks : this.props.tracks,
+      score : 0,
+      round : 1
+    };
+
+  }
+
+  hasAnswered(correctAnswer) {
+    console.log("In has Answered")
+    console.log("correctAnswer : ", correctAnswer)
+    if(correctAnswer) {
+      this.setState({score : this.state.score + 10});
+    }
+    else {
+      this.setState({score : this.state.score});
+    }
+    this.state.round++;
+  }
+
+  render() {
+    let blindtestBody;
+
+    // replace by the number of rounds
+    if(this.state.round < 6) {
+      let possibleTracks = []
+      let rightTrack = this.state.tracks.splice(Math.floor(Math.random()*this.state.tracks.length), 1)[0];
+      possibleTracks = [[rightTrack, true]];
+      for(let i = 1; i < 5; i++) {
+        let trackToAdd = this.state.tracks[Math.floor(Math.random()*this.state.tracks.length)];
+        possibleTracks.splice(Math.floor(Math.random()*(possibleTracks.length + 1)), 0, [trackToAdd, false])
+      }
+      
+      blindtestBody =
+        <div>
+          <h2>Round number {this.state.round}</h2>
+          <BlindtestRound 
+            tracks={possibleTracks}
+            url={rightTrack.audioLink}
+            onResponse={this.hasAnswered}
+          />
+        </div>
+    }
+    else {
+      blindtestBody = <h2> You have finished the blindtest </h2>
+    }
+    return(
+      <div>
+        {blindtestBody}
+        <h2> Your score is {this.state.score}</h2>
+      </div>
+    )
+  }
+}
+
+class BlindtestRound extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("In blindtest round constructor")
+    console.log("this props track : ", this.props.track)
+    this.hasAnswered = this.hasAnswered.bind(this);
+  }
+
+  hasAnswered(event) {
+    console.log("Right Anwser");
+    console.log("Event target:", event.target)
+    console.log("Event target value : ", event.target.value)
+    if(event.target.value === "true") {
+      this.props.onResponse(true);
+    }
+    else {
+      this.props.onResponse(false);
+    }
+  }
+
+  render() {
+    const tracks = this.props.tracks;
+    console.log("BlindtestRound render")
+    console.log("tracks :", tracks)
+
+    let tracksChoice = [];
+    for(let i = 0; i < tracks.length; i++) {
+      tracksChoice.push(<button onClick={this.hasAnswered} value={tracks[i][1]}>{tracks[i][0].artist} - {tracks[i][0].name}</button>)
+    }
+
+    return(
+      <div>
+        <TrackPlayer url={this.props.url}/>
+        {tracksChoice}
+      </div>
+    )
+  }
+}
+
+class TrackPlayer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.changePlayMode = this.changePlayMode.bind(this);
+    this.resetTrack = this.resetTrack.bind(this);
+    this.state = { playStatus : Sound.status.PLAYING, playMessage : "PAUSE"};
+  }
+
+  changePlayMode() {
+    if(this.state.playStatus === Sound.status.PLAYING) {
+      this.setState({ playStatus : Sound.status.PAUSED, playMessage : "PLAY"});
+    } else {
+      this.setState({ playStatus : Sound.status.PLAYING, playMessage : "PAUSE"});
+    }
+  }
+
+  resetTrack() {
+    this.setState({ playStatus : Sound.status.STOPPED, playMessage : "PLAY"});
+  }
+
+  render() {
+    const urlToPlay = this.props.url;
+    
+    return (
+      <div className="trackPlayer">
+        <Sound url={urlToPlay} playStatus={this.state.playStatus}/>
+        <button onClick={this.changePlayMode}>{this.state.playMessage}</button>
+        <button onClick={this.resetTrack}>RESET TRACK</button>
+      </div>
+    );
+  }
+}
+
 function TrackItem(props) {
   // Correct! There is no need to specify the key here:
   return (
@@ -130,13 +283,15 @@ function TrackTable(props) {
   );
   return (
     <table>
-      <tr>
-        <th>Track ID</th>
-        <th>Name</th>
-        <th>Artist</th> 
-        <th>Audio Link</th>
-      </tr>
-      {trackItems}
+     <tbody>
+        <tr>
+          <th>Track ID</th>
+          <th>Name</th>
+          <th>Artist</th> 
+          <th>Audio Link</th>
+        </tr>
+        {trackItems}
+      </tbody>
     </table>
   );
 }
@@ -146,6 +301,9 @@ function extractTracksDatas(response) {
   console.log('In extract, data :', data)
   console.log('In extract, data.items :', data.items)
   let dataToReturn = [];
+  if(data.items === undefined) {
+    return dataToReturn;
+  }
   for(let i = 0; i < data.items.length; i++) {
     let dataToAdd = { 
       id : data.items[i]["track"]["id"],
